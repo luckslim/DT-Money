0f5d185aeaf8e00b3315a5ff50a35959babf691f
Lucas <lucaslima78@hotmail.com>
2025-01-30 07:46:44 -0300

    up

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,28 +2,44 @@ import * as  Dialog from "@radix-ui/react-dialog"
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./style";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import * as z from 'zod';
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { api } from "../../lib/axios";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
     price: z.number(),
     category: z.string(),
-    // type: z.enum(['income', 'outcome']),
+    type: z.enum(['income', 'outcome']),
 })
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 export function NewTransactionsModal() {
+    const { createTransaction} = useContext(TransactionsContext)
+
     const {
+        reset,
+        control,
         register,
         handleSubmit,
-        formState:{isSubmitting}
+        formState: { isSubmitting }
     } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema)
     })
 
-    async function handleCreateNewTransaction(data: NewTransactionFormInputs){
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        console.log(data)
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        // await new Promise(resolve => setTimeout(resolve, 2000))
+        // console.log(data)
+        const { description, price, category, type } = data;
+
+        await createTransaction({
+            description,
+            price,
+            category,
+            type,
+        })
+        reset();
     }
     return (
 
@@ -33,22 +49,34 @@ export function NewTransactionsModal() {
                 <Dialog.Title>Nova Transação</Dialog.Title>
                 <CloseButton>< X size={20} /> </CloseButton>
                 <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
-                    <input 
-                     {...register('description')} type="text" placeholder="Descrição" required />
-                    <input 
-                     {...register('price',{ valueAsNumber: true })}  type="number" placeholder="Preço" required />
-                    <input 
-                     {...register('category')} type="text" placeholder="Categoria" required />
-                    <TransactionType>
-                        <TransactionTypeButton variant="income" value="income">
-                            <ArrowCircleUp size={24} />
-                            Entradas
-                        </TransactionTypeButton>
-                        <TransactionTypeButton variant="outcome" value="outcome">
-                            <ArrowCircleDown size={24} />
-                            Saídas
-                        </TransactionTypeButton>
-                    </TransactionType>
+                    <input
+                        {...register('description')} type="text" placeholder="Descrição" required />
+                    <input
+                        {...register('price', { valueAsNumber: true })} type="number" placeholder="Preço" required />
+                    <input
+                        {...register('category')} type="text" placeholder="Categoria" required />
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={(field) => {
+                            console.log(field)
+                            return (
+                                <TransactionType 
+                                onValueChange={field.field.onChange} 
+                                value={field.field.value}>
+                                    <TransactionTypeButton variant="income" value="income">
+                                        <ArrowCircleUp size={24} />
+                                        Entradas
+                                    </TransactionTypeButton>
+                                    <TransactionTypeButton variant="outcome" value="outcome">
+                                        <ArrowCircleDown size={24} />
+                                        Saídas
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            );
+                        }}
+                    />
+
                     <button type="submit" disabled={isSubmitting}>Cadastrar</button>
                 </form>
             </Content>
--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
-import { createContext,ReactNode, useEffect, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 interface Transaction {
     id: number;
     description: string;
@@ -7,28 +8,57 @@ interface Transaction {
     category: string;
     createdAt: string;
 }
-interface TransactionsContextType{
-    transactions: Transaction[];
+interface CreateTransactionInput {
+    description: string;
+    price:number;
+    category: string;
+    type:'income'|'outcome';
 }
-interface TransactionsProviderProps{
+interface TransactionsContextType {
+    transactions: Transaction[];
+    fetchTransactions: (query?: string) => Promise<void>;
+    createTransaction: (data: CreateTransactionInput)=> Promise<void>;
+}
+interface TransactionsProviderProps {
     children: ReactNode;
 }
+
 export const TransactionsContext = createContext({} as TransactionsContextType);
 
-export function TransactionsProvider({children}: TransactionsProviderProps){
-        const [ transactions, setTransactions ] = useState<Transaction[]>([]);
-    
-        async function loadTransactions() {
-            const response = await fetch('http://localhost:3000/transactions')
-            const data = await response.json();
-            setTransactions(data);
-        }
-    
-        useEffect(() => {
-            loadTransactions()
-        }, [])
-    return(
-        <TransactionsContext.Provider value={{ transactions }}>
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    async function fetchTransactions(query?: string) {
+        const response = await api.get('transactions', {
+            params: {
+                _sort: 'createdAt',
+                _order: 'desc',
+                description: query,
+            }
+        })
+        setTransactions(response.data);
+    }
+    async function createTransaction(data: CreateTransactionInput) {
+        const { description, price, category, type } = data;
+
+        const response = await api.post('transactions', {
+            description,
+            price,
+            category,
+            type,
+            createdAt: new Date(),
+        })
+        setTransactions(state => [response.data, ...state ])
+    }
+    useEffect(() => {
+        fetchTransactions()
+    }, [])
+    return (
+        <TransactionsContext.Provider value={{
+            transactions,
+            fetchTransactions,
+            createTransaction,
+        }}>
             {children}
         </TransactionsContext.Provider>
     )
